38f79a4da234dd75c8914eeaf6a840b68eb8d767
NatC02 <NatanCeballos02@gmail.com>
2021-03-18 19:50:12 -0400

    adding audio via node module

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.scss";
+import MicRecorder from "mic-recorder-to-mp3";
+import React from "react";
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
@@ -14,6 +16,42 @@ function App() {
       </header>
     </div>
   );
+}
+
+const Mp3Recorder = new MicRecorder({ bitRate: 128 });
+
+class recordAudio extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isRecording: false,
+      blobURL: "",
+      isBLocked: false,
+    };
+  }
+
+  start = () => {
+    if (this.state.isBLocked) {
+      console.log("Permission Denied");
+    } else {
+      Mp3Recorder.start()
+        .then(() => {
+          this.setState({ isRecording: true });
+        })
+        .catch((e) => console.error(e));
+    }
+  };
+
+  stop = () => {
+    Mp3Recorder.stop().getMp3()
+    .then(([buffer, blob]) =>  {
+      const blobURL = URL.createObjectURL(blob)
+      this.state({blobURL, isRecording: false});
+    }).catch((e) => console.log(e));
+  };
+
+  
+
 }
 
 export default App;

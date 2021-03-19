684eb04cf3cc9a0a400a80d74320aa6a61c1f19d
NatC02 <NatanCeballos02@gmail.com>
2021-03-18 22:15:48 -0400

    Created an instance to automatically play the audio once button stop is clicked, adding function to play video as well

--- src/App.js
+++ src/App.js
@@ -2,36 +2,20 @@ import "./App.scss";
 import MicRecorder from "mic-recorder-to-mp3";
 import React from "react";
 
-function App(props) {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="text">
-          {" "}
-          Phase 1: Once the button below is clicked, it will gather your video
-          and audio. It will output an mp3 video and a mp4 video that is 30
-          seconds long.
-        </h1>
-        <button className="btn customButton">Click here to gather</button>
-      </header>
-    </div>
-  );
-}
-
 const Mp3Recorder = new MicRecorder({ bitRate: 128 });
 
-class recordAudio extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isRecording: false,
       blobURL: "",
-      isBLocked: false,
+      isBlocked: false,
     };
   }
 
   start = () => {
-    if (this.state.isBLocked) {
+    if (this.state.isBlocked) {
       console.log("Permission Denied");
     } else {
       Mp3Recorder.start()
@@ -43,15 +27,84 @@ class recordAudio extends React.Component {
   };
 
   stop = () => {
-    Mp3Recorder.stop().getMp3()
-    .then(([buffer, blob]) =>  {
-      const blobURL = URL.createObjectURL(blob)
-      this.state({blobURL, isRecording: false});
-    }).catch((e) => console.log(e));
+    Mp3Recorder.stop()
+      .getMp3()
+      .then(([buffer, blob]) => {
+        //This will gather the audio and immediately play it once it is stopped
+
+        const file = new File(buffer, "me-at-thevoice.mp3", {
+          type: blob.type,
+          lastModified: Date.now(),
+        });
+
+        const player = new Audio(URL.createObjectURL(file));
+        player.play();
+
+        const blobURL = URL.createObjectURL(blob);
+        this.setState({ blobURL, isRecording: false });
+      })
+      .catch((e) => console.log(e));
   };
 
-  
+  componentDidMount() {
+    navigator.getUserMedia(
+      { audio: true },
+      () => {
+        console.log("Permission Granted");
+        this.setState({ isBlocked: false });
+      },
+      () => {
+        console.log("Permission Denied");
+        this.setState({ isBlocked: true });
+      }
+    );
+  }
 
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="text">
+            {" "}
+            Phase 1: Once the button below is clicked, it will gather your video
+            and audio. It will output an mp3 video and a mp4 video that is 30
+            seconds long.
+          </h1>
+          <button
+            className="btnAudioVideo customButton"
+            onClick={this.start}
+            disabled={this.state.isRecording}
+          >
+            Click here to gather audio
+          </button>
+          <button
+            className="btnAudioVideo customButton"
+            onClick={this.stop}
+            disabled={!this.state.isRecording}
+          >
+            Click here to stop gathering audio
+          </button>
+
+          <audio src={this.state.blobURL} controls="controls" />
+
+          <button
+            className="btnAudioVideo customButton"
+            onClick={this.start}
+            disabled={this.state.isRecording}
+          >
+            Click here to gather video
+          </button>
+          <button
+            className="btnAudioVideo customButton"
+            onClick={this.start}
+            disabled={this.state.isRecording}
+          >
+            Click here to stop gathering video
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
